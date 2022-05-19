643c7e813e6f4037ab07063c8133b909b4a6b013
Dhanalakshmi C <dhana2552@gmail.com>
2022-05-19 21:43:23 +0530

    Add files via upload

---
+++ valid_palindrome.py
@@ -0,0 +1,20 @@
+# Easy
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_str = ""
+        for char in s:
+            if char.isalnum():
+                new_str += char.lower()
+        return new_str == new_str[::-1]
+    
+test = Solution()
+print(test.isPalindrome("A man, a plan, a canal: Panama"))
